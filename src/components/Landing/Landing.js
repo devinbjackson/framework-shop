eadde6422d4a0b2267ce672bc4ba0d3732992394
Devin Jackson <Devidu@Devins-MacBook-Pro.local>
2017-10-24 15:07:42 -0500

    Done

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,19 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
-
+import FeaturedProduct from './FeaturedProduct/FeaturedProduct.js';
 import "./Landing.css";
-
+import {Link} from 'react-router-dom'
 import { addToCart } from "../../ducks/product";
 
 export function Landing( { addToCart, featuredProducts } ) {
+	var products = featuredProducts.map(function(product){
+		return <FeaturedProduct
+		addToCart={ () => addToCart( product.id ) }
+		description={ product.description }
+		key={ product.id }
+		logo={ product.logo }
+		name={ product.name }
+		onSale={ product.onSale }
+		price={ product.price }
+	  />
+	})
 	return (
 		<main className="landing">
 			<h1>Featured Products</h1>
 			<div className="landing__products-wrapper">
-
+				{products}
 			</div>
 
-			<h1 className="landing__full-shop-link">Take me to the full shop!</h1>
+			<Link to="/shop" className="landing__full-shop-link"><h1 className="landing__full-shop-link">Take me to the full shop!</h1></Link>
 		</main>
 	);
 }
